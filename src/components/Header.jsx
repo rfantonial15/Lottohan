a5ff8adc212b601ddf05f9bfafe5dd8b97a9cd20
Randulf Fantonial <fantonial.randulf9@gmail.com>
2024-05-29 01:01:59 +0800

    neww

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,10 +7,10 @@ const Header = () => {
     <header className="bg-white p-4 flex justify-between items-center mx-80">
       <div className="text-xl font-bold">
         <img src={logo} alt="Lottery Logo" className="h-10 inline-block mr-2" />
-        Lottery
+        Swertres ni Franzyn
       </div>
       <nav>
-        <a href="#" className="text-blue-500 mx-2">
+        <a href="#" className="text-blue-500  mx-2">
           Home
         </a>
         <a href="#" className="text-blue-500 mx-2">
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import './css/style.css'; // Import the CSS file
 
 const MainContent = () => {
   return (
-    <div className="p-8 grid grid-cols-2 gap-4 mx-80">
-      <div className="bg-white p-32 rounded shadow-md"></div>
-      <div className="bg-white p-32 rounded shadow-md"></div>
-      <div className="bg-white p-32 rounded shadow-md"></div>
-      <div className="bg-white p-32 rounded shadow-md"></div>
+    <div className="main-content-container">
+      <div className="content-box">
+        <div className="content-inner-box">
+          <Link to="/bet" className="content-link">Start Betting</Link>
+        </div>
+      </div>
+      <div className="content-box">
+        <div className="content-inner-box">
+          <h3 className="content-title">RESULT DIRI</h3>
+          <p className="content-desc">2:00 PM Draw | Cut-Off Time: 4:45 PM</p>
+        </div>
+      </div>
+      <div className="content-box">
+        <div className="content-inner-box">
+          <h3 className="content-title">RESULT DIRI</h3>
+          <p className="content-desc">5:00 PM Draw | Cut-Off Time: 4:45 PM</p>
+        </div>
+      </div>
+      <div className="content-box">
+        <div className="content-inner-box">
+          <h3 className="content-title">RESULT DIRI</h3>
+          <p className="content-desc">9:00 PM Draw | Cut-Off Time: 8:45 PM</p>
+        </div>
+      </div>
     </div>
   );
 };
